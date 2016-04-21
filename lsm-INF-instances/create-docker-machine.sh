a5aa1be754b296b75fff6f09c21a3b783af59c51
root <root@mac-debian>
2016-04-21 17:38:59 -0500

    Add files for lsm-INF-instances

---
+++ lsm-INF-instances/create-docker-machine.sh
@@ -0,0 +1,12 @@
+source ../awsvars.sh
+
+#export AWS_VPC_ID=vpc-055fe160
+#export AWS_SECURITY_GROUP=sg-d6e278b3
+#export AWS_SUBNET_ID=subnet-b2f0afd7
+
+export AWS_SECURITY_GROUP=docker-machine-INF
+export AWS_VPC_ID=vpc-f1cc0f94
+export AWS_DEFAULT_REGION=us-west-2
+export AWS_ZONE=a
+
+docker-machine -D create --driver amazonec2 --amazonec2-root-size "200" --amazonec2-iam-instance-profile "aws-cloudwatch-logs-variables" $1
