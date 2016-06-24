efdb1ea85187a955237d9f0619ead2f9a1d4c722
root <root@mac-debian>
2016-06-24 07:47:20 -0500

    Update settings

---
+++ awslogs/aws-sp-wba-xx-logs.sh
@@ -0,0 +1,1 @@
+awslogs get pushtravel-latam pushtravel-wba-latam-$1 --watch --s1m
--- lsm-INF-instances/lsm-INF-dckr-001/lsm-INF-dckr-nagios-core-4.x.sh
+++ lsm-INF-instances/lsm-INF-dckr-001/lsm-INF-dckr-nagios-core-4.x.sh
@@ -2,22 +2,7 @@
 
 docker build -t nagios-core-4.x:latest ./lsm-INF-dckr-nagios-core-4.x
 
-
 docker stop lsm-INF-dckr-nagios-core-4.x
 docker rm lsm-INF-dckr-nagios-core-4.x
 
-
 docker run --name=lsm-INF-dckr-nagios-core-4.x -d -p 8025:25 -p 8080:80 --restart=always --log-driver=awslogs --log-opt awslogs-region=us-west-2 --log-opt awslogs-group=lsm-INF-logs --log-opt awslogs-stream=lsm-INF-dckr-nagios-core-4.x nagios-core-4.x:latest
-
-
-
-
-
-# Model
-#------
-#docker build -t nginx-z0:latest ./nginx-z0
-
-#docker stop nginx-z0
-#docker rm nginx-z0
-
-#docker run -d -p 80:80 -p 443:443 --restart=always --name=nginx-z0 nginx-z0:latest
