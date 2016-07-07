386f84ceec76438073425e3c6eb822e0db916fe1
root <root@mac-debian>
2016-07-07 04:08:16 -0500

    Add jenkins settings

---
+++ lsm-INF-instances/lsm-INF-dckr-001/lsm-INF-dckr-jenkins-ver1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker tag jenkins jenkins-ver1:rollback
+
+docker pull jenkins
+#docker build -t nagios-core-4.x:latest ./lsm-INF-dckr-nagios-core-4.x
+
+docker stop jenkins-ver1
+docker rm jenkins-ver1
+
+docker run --name=jenkins-ver1 -d -p 8010:8080 -p 50000:50000 --restart=always --log-driver=awslogs --log-opt awslogs-region=us-west-2 --log-opt awslogs-group=lsm-INF-logs --log-opt awslogs-stream=lsm-INF-dckr-jenkins-ver1 jenkins
