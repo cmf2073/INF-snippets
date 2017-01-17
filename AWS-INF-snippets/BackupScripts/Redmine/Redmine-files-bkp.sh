4a719649d5700e91676d323bbfa8ddd00a5f0116
carlos.farina <carlos.farina@claroviajes.com>
2017-01-17 15:49:46 +0000

    Update redmine file-repo scroipt

--- AWS-INF-snippets/BackupScripts/Redmine/Redmine-files-bkp.sh
+++ AWS-INF-snippets/BackupScripts/Redmine/Redmine-files-bkp.sh
@@ -6,7 +6,7 @@ design_source="/var/www/design.lms.mx/public/"
 
 
 ## Destinos del sync en AWS-S3 bucket
-S3redmine_bucket="s3://s3sysadmin2/lsm-INF-001/redmine-backups"
+S3redmine_bucket="s3://s3sysadmin2/lsm-INF-001/redmine-bkups"
 S3redmine_day_folder="Redmine-filerepo-bkup-"$(date +%A)
 design_day_folder="Design-Backup-"$(date +%A)
 
@@ -21,14 +21,14 @@ echo " .. "
 echo " ... "
 echo " ========== ========== ========== ========== ========= "
 echo " ========== ========== ========== ========== ========= "
-echo " === $start1 - Comienza la copia de seguridad de archivos de Redmine de $redmine_source a $redmine_day_folder ."
+echo " === $start1 - Starting backup of Redmine files Repo from $redmine_source to $S3redmine_bucket/$S3redmine_day_folder/."
 
 /usr/bin/s3cmd sync --delete-removed $redmine_source $S3redmine_bucket/$S3redmine_day_folder/
 
 
 ## Linea para log
-start2=$(date)
-echo " === $start2 - Comienza la copia de seguridad de archivos de design.lsm.com de $design_source a $design_day_folder ."
+#start2=$(date)
+#echo " === $start2 - Comienza la copia de seguridad de archivos de design.lsm.com de $design_source a $design_day_folder ."
 
-/usr/bin/s3cmd sync --delete-removed $design_source s3://s3bucket001-redmine/$design_day_folder/
+#/usr/bin/s3cmd sync --delete-removed $design_source s3://s3bucket001-redmine/$design_day_folder/
 
